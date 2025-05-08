2d67d964af06718b3932dffe7a55082ce5903ac9
Victor Shpyrka <victor.shpyrka@gmail.com>
2025-05-08 13:55:35 +0000

    chore: bump version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.vshpyrka.playground"
         minSdk = sdk.versions.minSdk.get().toInt()
         targetSdk = sdk.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
